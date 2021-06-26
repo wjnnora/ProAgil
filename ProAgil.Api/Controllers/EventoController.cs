2dc3fd986673cdb22a22992ce137af1629e145c5
wjnnora <wellington_nora94@hotmail.com>
2021-06-26 15:25:40 -0300

    feat(EventoController): adiciona nova controller

---
+++ ProAgil.Api/Controllers/EventoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProAgil.Api.Data;
+using ProAgil.Api.Model;
+
+namespace ProAgil.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    { 
+        private readonly ILogger<EventoController> _logger;
+        private readonly DataContext context;   
+
+        public EventoController(ILogger<EventoController> logger, DataContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {            
+            return context.Eventos.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {                        
+            return context.Eventos.Where(x => x.EventoId == id).ToList();
+        }        
+    }
+}
