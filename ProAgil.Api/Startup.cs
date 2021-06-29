665b051e0980f8fefe48d7c332d7a2a7a4b15395
wjnnora <wellington_nora94@hotmail.com>
2021-06-29 01:21:02 -0300

    feat(*): aplica atualizacoes de Cors

--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -29,7 +29,7 @@ namespace ProAgil.Api
         public void ConfigureServices(IServiceCollection services)
         {                        
             services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddControllers();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +41,8 @@ namespace ProAgil.Api
             }
 
             //app.UseHttpsRedirection();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseRouting();
 
