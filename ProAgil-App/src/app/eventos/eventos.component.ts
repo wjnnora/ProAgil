1245e790c4f3f5f9b15626149c32233a4ce47f26
wjnnora <wellington_nora94@hotmail.com>
2021-07-01 20:02:25 -0300

    feat: tipa a propriedade eventos

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any;
+  eventos: any = [];
   url: string = 'http://localhost:5000/Evento/';
     
 
