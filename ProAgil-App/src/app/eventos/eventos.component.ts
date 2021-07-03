62baf2eb9aa9d138973157c9c9a30c349d547104
wjnnora <wellington_nora94@hotmail.com>
2021-07-03 19:22:51 -0300

    feat: adiciona o service no app

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { EventoService } from './../service/evento.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,22 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [];
-  url: string = 'http://localhost:5000/Evento/';
-    
+  eventos: any = [];  
+  url: string = 'http://localhost:5000/Evento/';    
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private eventoService: EventoService) { }
 
   ngOnInit() {
-    this.getEventos();
+    this.getEventos();          
   }
 
   getEventos(){
-    this.http.get('http://localhost:5000/Evento/').subscribe(response => {
+    this.eventoService.getAllEventos().subscribe(response => {
       this.eventos = response;
     }, error => {
       console.log(error);
-    });    
-  }
-
+    });   
+  }  
 }
