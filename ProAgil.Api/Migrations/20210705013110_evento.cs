2065ddf20d08536d6f2d604381f9020ccd474a6c
wjnnora <wellington_nora94@hotmail.com>
2021-07-04 23:03:42 -0300

    feat: adiciona migration

---
+++ ProAgil.Api/Migrations/20210705013110_evento.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.Api.Migrations
+{
+    public partial class evento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Eventos",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Eventos");
+        }
+    }
+}
