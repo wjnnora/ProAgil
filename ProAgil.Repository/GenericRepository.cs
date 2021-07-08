8c1b946563a915ae157f6226fab8af2b5486bf6e
wjnnora <wellington_nora94@hotmail.com>
2021-07-08 09:53:59 -0300

    refactor: faz alteracoes no repositorio generico

--- ProAgil.Repository/GenericRepository.cs
+++ ProAgil.Repository/GenericRepository.cs
@@ -17,40 +17,39 @@ namespace ProAgil.Repository
         /// Save the entity.
         /// </summary>        
         /// <param name="Entity">The entity that will be saved.</param>
-        public void Insert(T Entity)
+        /// <return>
+        /// Return a boolean that indicating wheter the insertion was completed successfully.
+        /// </return>
+        public async Task<bool> Insert(T Entity)
         {
-            _context.Add(Entity);
+            _context.Set<T>().Add(Entity);
+            return await _context.SaveChangesAsync() > 0;
         }
 
         /// <summary>
         /// Delete the entity.
         /// </summary>        
         /// <param name="Entity">The entity that will be deleted.</param>
-        public virtual void Delete(T Entity)
+        /// <return>
+        /// Return a boolean that indicating wheter the deletion was completed successfully.
+        /// </return>
+        public async Task<bool> Delete(T Entity)
         {
-            _context.Remove(Entity);
+            _context.Set<T>().Attach(Entity);
+            _context.Entry(Entity).State = EntityState.Deleted;
+            return await _context.SaveChangesAsync() > 0;
         }
 
         /// <summary>
         /// Update the entity.
         /// </summary>        
         /// <param name="Entity">The entity that will be updated.</param>
-        public void Update(T Entity)
+        public async Task<bool> Update(T Entity)
         {
             _context.Set<T>().Attach(Entity);
-            _context.Entry(Entity).State = EntityState.Modified;            
-        }
-
-        /// <summary>
-        /// Save changes.
-        /// </summary>    
-        /// <returns>
-        /// Return a boolean that indicates whether changes was completed successfully. 
-        /// </returns> 
-        public async Task<bool> SaveChangesAsync()
-        {
+            _context.Entry(Entity).State = EntityState.Modified;
             return await _context.SaveChangesAsync() > 0;
-        }        
+        }            
 
         public async Task<bool> Exists(int id)
         {            
