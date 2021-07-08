8d8f6c9a2e0415d813d9b5eed5145d269d942775
wjnnora <wellington_nora94@hotmail.com>
2021-07-07 21:34:14 -0300

    feat: remove o sets desnecessarios de propriedades

--- ProAgil.Domain/PalestranteEvento.cs
+++ ProAgil.Domain/PalestranteEvento.cs
@@ -3,8 +3,8 @@ namespace ProAgil.Domain
     public class PalestranteEvento
     {
         public int EventoId { get; set; }
-        public Evento Evento { get; set; }
+        public Evento Evento { get; }
         public int PalestranteId { get; set; }
-        public Palestrante Palestrante { get; set; }
+        public Palestrante Palestrante { get; }
     }
 }
