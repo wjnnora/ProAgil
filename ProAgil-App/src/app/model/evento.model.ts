152572bda1d43be00e6547de78d5628955d1612d
wjnnora <wellington_nora94@hotmail.com>
2021-07-09 12:30:09 -0300

    refactor: atualiza o nome do diretorio de services

--- ProAgil-App/src/app/model/evento.model.ts
+++
@@ -1,9 +0,0 @@
-                                                                                                                                                                                                                                                                                                                                                            export interface Evento{
-    eventoId: number,
-    local: string,
-    dataEvento: string,
-    tema: string,
-    qtdPessoas: number,
-    lote: string
-    imagePath: string
-}
