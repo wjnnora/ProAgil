ebfb7355f6ed5bbf1771b5202f254fe30b34b730
wjnnora <wellington_nora94@hotmail.com>
2021-08-17 16:31:39 -0300

    feat(routing): configura as rotas

--- ProAgil-App/src/app/app-routing.module.ts
+++ ProAgil-App/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ContatosComponent } from './contatos/contatos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './eventos/eventos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent },
+  { path: 'palestrantes', component: PalestrantesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
