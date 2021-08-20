36e7bc5452030ef5722fef025d2d80d43ed54e41
wjnnora <wellington_nora94@hotmail.com>
2021-08-20 15:06:41 -0300

    feat(Context): faz a relacao das entitidades que serao utilizadas pelo entity

--- ProAgil.Repository/ProAgilContext.cs
+++ ProAgil.Repository/ProAgilContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProAgil.Repository
 {
-    public class ProAgilContext: DbContext
+    public class ProAgilContext: IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {    
         public ProAgilContext(DbContextOptions<ProAgilContext> options) : base(options) {}        
 
@@ -15,6 +18,23 @@ namespace ProAgil.Repository
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole => {
+
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId});
+
+                userRole.HasOne(r => r.Role)
+                    .WithMany(ur => ur.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(u => u.User)
+                    .WithMany(ur => ur.UserRoles)
+                    .HasForeignKey(u => u.UserId)
+                    .IsRequired();
+            });
+
             modelBuilder.Entity<PalestranteEvento>()
                 .HasKey(pe => new { pe.EventoId, pe.PalestranteId });
         }        
