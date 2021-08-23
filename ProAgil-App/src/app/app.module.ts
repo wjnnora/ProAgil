1adf66a98a1550a43b582b46f6007b074dbfd119
wjnnora <wellington_nora94@hotmail.com>
2021-08-23 14:45:44 -0300

    feat(module): adiciona os novos componentes no module

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -20,10 +20,13 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContatosComponent } from './contatos/contatos.component';
 import { TituloComponent } from './_shared/titulo/titulo.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 
 
 @NgModule({
-  declarations: [					
+  declarations: [						
     AppComponent,
     EventosComponent,
     NavComponent,
@@ -31,7 +34,10 @@ import { TituloComponent } from './_shared/titulo/titulo.component';
     PalestrantesComponent,
     DashboardComponent,
     ContatosComponent,
-    TituloComponent
+    TituloComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
    ],
   imports: [
     BrowserModule,
