f2b8a4b0a7d88730e3b16b14d52266a5b8d10456
wjnnora <wellington_nora94@hotmail.com>
2021-08-23 16:13:57 -0300

    feat(auth): adiciona o service de user

---
+++ ProAgil-App/src/app/_service/auth.service.ts
@@ -0,0 +1,38 @@
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  jwtHelper = new JwtHelperService();
+  decodeToken: any;
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(`${environment.apiUrl}/user/login`, model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodeToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(`${environment.apiUrl}/user/register`, model);
+  }
+
+  loggedIn() {
+    const token = JSON.parse(localStorage.getItem('token')!);
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
+}
