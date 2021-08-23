e8a9790a84546ee61f7067e14e1945a9ac1be3cd
wjnnora <wellington_nora94@hotmail.com>
2021-08-23 14:45:13 -0300

    feat(routing): configura a rota dos novos componentes

--- ProAgil-App/src/app/app-routing.module.ts
+++ ProAgil-App/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { ContatosComponent } from './contatos/contatos.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'user', component: UserComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'registration', component: RegistrationComponent }
+  ] },
   { path: 'eventos', component: EventosComponent },
   { path: 'palestrantes', component: PalestrantesComponent },
   { path: 'dashboard', component: DashboardComponent },
