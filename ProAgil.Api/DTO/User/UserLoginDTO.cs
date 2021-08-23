f1cfba130cc536703cbe9dc2f54b5c40d9c1e156
wjnnora <wellington_nora94@hotmail.com>
2021-08-23 11:24:56 -0300

    feat(UserLoginDTO): cria a model de login

---
+++ ProAgil.Api/DTO/User/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+namespace ProAgil.Api.DTO.User
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; }        
+        public string Password { get; set; }        
+    }
+}
