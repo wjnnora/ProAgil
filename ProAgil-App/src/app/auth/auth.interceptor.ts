748dcfdd5dab57fa42c1a6a8ccc58e833c3efb2c
wjnnora <wellington_nora94@hotmail.com>
2021-08-24 14:00:31 -0300

    feat(interfaceptor): adiciona o interceptor

---
+++ ProAgil-App/src/app/auth/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable} from "@angular/core";
+import { Router } from "@angular/router";
+import { tap } from "rxjs/internal/operators/tap";
+
+@Injectable({ providedIn: 'root' })
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private route: Router) { }
+    
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (localStorage.getItem('token') !== null) {
+            const cloneReq = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`)
+            });
+            return next.handle(cloneReq).pipe(
+                tap(
+                    success => { },
+                    error => {
+                        if (error.status == 401) {
+                            this.route.navigateByUrl('user/login');
+                        }
+                    }
+                )
+            );
+        }
+        return next.handle(req.clone());
+    }
+}
